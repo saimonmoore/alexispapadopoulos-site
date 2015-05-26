77b75ca5b4041aaf93f8ba0727b38bbdfc4add70
Saimon Moore <saimonmoore@gmail.com>
2015-05-26 23:31:31 +0200

    content page is for content only

--- web/app/themes/alexispapadopoulos/templates/content-projects.php
+++ web/app/themes/alexispapadopoulos/templates/content-projects.php
@@ -1,18 +1,14 @@
-<?php query_posts( 'category_name=projects' ); ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class('project'); ?>>
-    <header>
-      <h1 class="entry-title">[PROJECT]<?php the_title(); ?></h1>
-    </header>
-    <div class="showcase">
-      <?php echo do_shortcode('[gallery columns="2" size="medium"]'); ?>
-    </div>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-  </article>
-<?php endwhile; ?>
+<article <?php post_class('project'); ?>>
+  <header>
+    <h1 class="entry-title">[PROJECT]<?php the_title(); ?></h1>
+  </header>
+  <div class="showcase">
+    <?php echo do_shortcode('[gallery columns="2" size="medium"]'); ?>
+  </div>
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
+  <footer>
+    <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+  </footer>
+</article>

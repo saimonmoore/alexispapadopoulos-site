30a34a56ae3d70824da570e228358467aa0e3d92
Saimon Moore <saimonmoore@gmail.com>
2015-05-31 10:02:12 +0200

    Add custom meta data key project id

--- web/app/themes/alexispapadopoulos/templates/archive-projects.php
+++ web/app/themes/alexispapadopoulos/templates/archive-projects.php
@@ -2,6 +2,9 @@
 
 <article <?php post_class('project'); ?>>
   <header>
+    <span class='project_id'>
+      <?php $key="project_id"; echo get_post_meta($post->ID, $key, true); ?>.
+    </span>
     <h1 class="entry-title">
       <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
     </h1>
--- web/app/themes/alexispapadopoulos/templates/content.php
+++ web/app/themes/alexispapadopoulos/templates/content.php
@@ -1,6 +1,5 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title">[CONTENT]<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
